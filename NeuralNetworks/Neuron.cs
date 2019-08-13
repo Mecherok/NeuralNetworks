a2fe4ce50a9ad959710cf548c3261ef934fb024a
Mecherok <dmitrymeshcheryakov99@gmail.com>
2019-08-13 16:21:47 +0700

    Fix FeedForward

--- NeuralNetworks/Neuron.cs
+++ NeuralNetworks/Neuron.cs
@@ -22,7 +22,12 @@ namespace NeuralNetworks {
                 sum += inputs[i] * Weights[i];
             }
 
-            Output = Sigmoid(sum);
+            if (NeuronType != NeuronType.Input) {
+                Output = Sigmoid(sum);
+            } else {
+                Output = sum;
+            }
+
             return Output;
         }
 
